c864d8cba9172221fe46c113655db5510eb4f581
Staciiaz <chollakorn.nimp@gmail.com>
2025-05-26 17:14:39 +0700

    fix: merge same explaination output key into one instance

--- main.py
+++ main.py
@@ -22,15 +22,19 @@ def solve(input_string: str) -> tuple[int, list[str]]:
                 stack.append(current)
             else:
                 output_value += current_value - top_value
-                stack.pop()
                 explaination_list.append(f"{top}{current} = {current_value - top_value}")
+                stack.pop()
         else:
             stack.append(current)
-    while len(stack) > 0:
-        top = stack.pop()
-        top_value = VALUE_MAP[top]
-        output_value += top_value
-        explaination_list.insert(0, f"{top} = {top_value}")
+    if len(stack) > 0:
+        count_map = {k: 0 for k in VALUE_MAP.keys()}
+        for item in stack:
+            count_map[item] += 1
+        for key in count_map:
+            if count_map[key] > 0:
+                output_value += count_map[key] * VALUE_MAP[key]
+                output_key = "".join([key for _ in range(count_map[key])])
+                explaination_list.insert(0, f"{output_key} = {count_map[key] * VALUE_MAP[key]}")
     return output_value, explaination_list
 
 
