e90d906549ae9e4703b704b9307f76039f0f9b7b
Staciiaz <chollakorn.nimp@gmail.com>
2025-06-04 11:17:35 +0700

    refactor: simplify solve function by removing unnecessary stack operations

--- main.py
+++ main.py
@@ -10,30 +10,20 @@ VALUE_MAP = {
 
 
 def solve(input_string: str) -> tuple[int, list[str]]:
-    stack = []
-    item_list = []
-    for current in input_string:
-        if len(stack) > 0:
-            top = stack[-1]
-            current_value = VALUE_MAP[current]
-            top_value = VALUE_MAP[top]
-            if current_value <= top_value:
-                stack.append(current)
-            else:
-                item_list.append((f"{top}{current}", current_value - top_value))
-                stack.pop()
+    stack = [(input_string[0], VALUE_MAP[input_string[0]], input_string[0])]
+    for current in input_string[1:]:
+        current_value = VALUE_MAP[current]
+        top, top_value, top_root = stack[-1]
+        if current == top_root:
+            stack.pop()
+            stack.append((f"{top}{current}", top_value + current_value, top_root))
+        elif current_value < top_value:
+            stack.append((current, current_value, current))
         else:
-            stack.append(current)
-    if len(stack) > 0:
-        count_map = {k: 0 for k in VALUE_MAP.keys()}
-        for item in stack:
-            count_map[item] += 1
-        for key in count_map:
-            if count_map[key] > 0:
-                item_list.append(("".join([key for _ in range(count_map[key])]), VALUE_MAP[key] * count_map[key]))
-    item_list.sort(key=lambda x: -x[1])
-    output_value = sum(value for _, value in item_list)
-    explaination_list = [f"{item} = {value}" for item, value in item_list]
+            stack.pop()
+            stack.append((f"{top}{current}", current_value - top_value, top_root))
+    output_value = sum(value for _, value, _ in stack)
+    explaination_list = [f"{item} = {value}" for item, value, _ in stack]
     return output_value, explaination_list
 
 
